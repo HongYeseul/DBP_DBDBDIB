c0a6cc4ab4c5a40b2a6f6ed6604f5aec619b576c
Hyunji99Choi <tyg06229@gmail.com>
2020-11-09 18:01:49 +0900

    DBManager 추가

---
+++ DBDBDIB/DBDBDIB/DBManager.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBDBDIB
+{
+    class DBManager
+    {
+        private static DBManager instance = new DBManager();
+        private string strConn = "Server=49.50.174.201;Database=wep;Uid=dbdbdib;Pwd=s123123;Charset=utf8";
+        public static DBManager GetInstance() { return instance; }
+
+        //객체 생성 제한
+        private DBManager() { }
+
+        public void DBquery(string query)
+        {
+            using (MySqlConnection conn = new MySqlConnection(strConn))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public MySqlDataReader select(string query)
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(strConn);
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                //conn.Close();
+                return rdr;
+            }
+            catch
+            {
+                MessageBox.Show("time over");
+                return null;
+            }
+
+        }
+
+    }
+}
