3aec7ebf094d23baf539c7568fb308230c03a619
aream <arlee957790@gmail.com>
2020-11-10 15:43:31 +0900

    test

---
+++ DBDBDIB/DBDBDIB/SalaryDetail.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_1
+{
+    public partial class SalaryDetail : Form
+    {
+        public string ExtraHour_ {get; set;}
+
+        public SalaryDetail()
+        {
+            InitializeComponent();
+        }
+
+        private void tabControlSalary_Selected(object sender, EventArgs e)
+        {
+            if(tabControlSalary.SelectedTab == tabPageShowSalary)
+            {
+
+            }else if(tabControlSalary.SelectedTab == tabPageShowTax){
+
+            }
+        }
+
+        private void buttonInputExtra_Click(object sender, EventArgs e)
+        {
+            SalaryInputDialog dig = new SalaryInputDialog(this);
+            dig.Show();
+        }
+    }
+}
