519efafb3f7b225c0d37ab0ae10d15b54dc2e088
HongYeseul <ghd8119@naver.com>
2020-11-16 17:33:35 +0900

    1116 홍예슬 메시지메인폼 구현

--- DBDBDIB/DBDBDIB/MessageMainForm.Designer.cs
+++ DBDBDIB/DBDBDIB/MessageMainForm.Designer.cs
@@ -28,29 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txtSearchMsg = new System.Windows.Forms.TextBox();
-            this.btnDeleteMsg = new System.Windows.Forms.Button();
+            this.txtSearchWhoSent = new System.Windows.Forms.TextBox();
             this.btnNewMsg = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.검색하기 = new System.Windows.Forms.GroupBox();
+            this.radioBtnContent = new System.Windows.Forms.RadioButton();
+            this.radioBtnTitle = new System.Windows.Forms.RadioButton();
+            this.radioBtnWhoSent = new System.Windows.Forms.RadioButton();
+            this.txtSearchContent = new System.Windows.Forms.TextBox();
+            this.txtSearchTitle = new System.Windows.Forms.TextBox();
             this.btnSearch = new System.Windows.Forms.Panel();
+            this.검색하기.SuspendLayout();
             this.SuspendLayout();
             // 
-            // txtSearchMsg
+            // txtSearchWhoSent
             // 
-            this.txtSearchMsg.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.txtSearchMsg.Location = new System.Drawing.Point(25, 23);
-            this.txtSearchMsg.Name = "txtSearchMsg";
-            this.txtSearchMsg.Size = new System.Drawing.Size(138, 26);
-            this.txtSearchMsg.TabIndex = 0;
-            // 
-            // btnDeleteMsg
-            // 
-            this.btnDeleteMsg.Location = new System.Drawing.Point(210, 25);
-            this.btnDeleteMsg.Name = "btnDeleteMsg";
-            this.btnDeleteMsg.Size = new System.Drawing.Size(51, 23);
-            this.btnDeleteMsg.TabIndex = 2;
-            this.btnDeleteMsg.Text = "삭제";
-            this.btnDeleteMsg.UseVisualStyleBackColor = true;
+            this.txtSearchWhoSent.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtSearchWhoSent.Location = new System.Drawing.Point(132, 19);
+            this.txtSearchWhoSent.Name = "txtSearchWhoSent";
+            this.txtSearchWhoSent.Size = new System.Drawing.Size(95, 22);
+            this.txtSearchWhoSent.TabIndex = 0;
             // 
             // btnNewMsg
             // 
@@ -67,18 +64,87 @@
             this.listBox1.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(25, 65);
+            this.listBox1.Location = new System.Drawing.Point(25, 105);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(730, 364);
+            this.listBox1.Size = new System.Drawing.Size(730, 324);
             this.listBox1.TabIndex = 5;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // 검색하기
+            // 
+            this.검색하기.Controls.Add(this.radioBtnContent);
+            this.검색하기.Controls.Add(this.radioBtnTitle);
+            this.검색하기.Controls.Add(this.radioBtnWhoSent);
+            this.검색하기.Controls.Add(this.txtSearchContent);
+            this.검색하기.Controls.Add(this.txtSearchTitle);
+            this.검색하기.Controls.Add(this.txtSearchWhoSent);
+            this.검색하기.Controls.Add(this.btnSearch);
+            this.검색하기.Location = new System.Drawing.Point(25, 12);
+            this.검색하기.Name = "검색하기";
+            this.검색하기.Size = new System.Drawing.Size(625, 79);
+            this.검색하기.TabIndex = 7;
+            this.검색하기.TabStop = false;
+            this.검색하기.Text = "검색하기";
+            // 
+            // radioBtnContent
+            // 
+            this.radioBtnContent.AutoSize = true;
+            this.radioBtnContent.Location = new System.Drawing.Point(254, 20);
+            this.radioBtnContent.Name = "radioBtnContent";
+            this.radioBtnContent.Size = new System.Drawing.Size(91, 16);
+            this.radioBtnContent.TabIndex = 15;
+            this.radioBtnContent.TabStop = true;
+            this.radioBtnContent.Text = "내용 별 검색";
+            this.radioBtnContent.UseVisualStyleBackColor = true;
+            this.radioBtnContent.CheckedChanged += new System.EventHandler(this.radioBtnContent_CheckedChanged);
+            // 
+            // radioBtnTitle
+            // 
+            this.radioBtnTitle.AutoSize = true;
+            this.radioBtnTitle.Location = new System.Drawing.Point(15, 50);
+            this.radioBtnTitle.Name = "radioBtnTitle";
+            this.radioBtnTitle.Size = new System.Drawing.Size(91, 16);
+            this.radioBtnTitle.TabIndex = 14;
+            this.radioBtnTitle.TabStop = true;
+            this.radioBtnTitle.Text = "제목 별 검색";
+            this.radioBtnTitle.UseVisualStyleBackColor = true;
+            this.radioBtnTitle.CheckedChanged += new System.EventHandler(this.radioBtnTitle_CheckedChanged);
+            // 
+            // radioBtnWhoSent
+            // 
+            this.radioBtnWhoSent.AutoSize = true;
+            this.radioBtnWhoSent.Location = new System.Drawing.Point(15, 23);
+            this.radioBtnWhoSent.Name = "radioBtnWhoSent";
+            this.radioBtnWhoSent.Size = new System.Drawing.Size(115, 16);
+            this.radioBtnWhoSent.TabIndex = 8;
+            this.radioBtnWhoSent.TabStop = true;
+            this.radioBtnWhoSent.Text = "보낸사람 별 검색";
+            this.radioBtnWhoSent.UseVisualStyleBackColor = true;
+            this.radioBtnWhoSent.CheckedChanged += new System.EventHandler(this.radioBtnWhoSent_CheckedChanged);
+            // 
+            // txtSearchContent
+            // 
+            this.txtSearchContent.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtSearchContent.Location = new System.Drawing.Point(351, 17);
+            this.txtSearchContent.Name = "txtSearchContent";
+            this.txtSearchContent.Size = new System.Drawing.Size(95, 22);
+            this.txtSearchContent.TabIndex = 9;
+            // 
+            // txtSearchTitle
+            // 
+            this.txtSearchTitle.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtSearchTitle.Location = new System.Drawing.Point(132, 45);
+            this.txtSearchTitle.Name = "txtSearchTitle";
+            this.txtSearchTitle.Size = new System.Drawing.Size(95, 22);
+            this.txtSearchTitle.TabIndex = 7;
             // 
             // btnSearch
             // 
             this.btnSearch.BackgroundImage = global::DBDBDIB.Properties.Resources.search;
             this.btnSearch.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnSearch.Location = new System.Drawing.Point(169, 23);
+            this.btnSearch.Location = new System.Drawing.Point(504, 17);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(35, 26);
+            this.btnSearch.Size = new System.Drawing.Size(83, 47);
             this.btnSearch.TabIndex = 3;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
@@ -87,25 +153,29 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(802, 450);
+            this.Controls.Add(this.검색하기);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btnNewMsg);
-            this.Controls.Add(this.btnSearch);
-            this.Controls.Add(this.btnDeleteMsg);
-            this.Controls.Add(this.txtSearchMsg);
             this.Name = "MessageMainForm";
             this.Text = "MessageMainForm";
             this.Load += new System.EventHandler(this.MessageMainForm_Load);
+            this.검색하기.ResumeLayout(false);
+            this.검색하기.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtSearchMsg;
-        private System.Windows.Forms.Button btnDeleteMsg;
+        private System.Windows.Forms.TextBox txtSearchWhoSent;
         private System.Windows.Forms.Button btnNewMsg;
         private System.Windows.Forms.Panel btnSearch;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox 검색하기;
+        private System.Windows.Forms.RadioButton radioBtnContent;
+        private System.Windows.Forms.RadioButton radioBtnTitle;
+        private System.Windows.Forms.RadioButton radioBtnWhoSent;
+        private System.Windows.Forms.TextBox txtSearchContent;
+        private System.Windows.Forms.TextBox txtSearchTitle;
     }
 }
--- DBDBDIB/DBDBDIB/MessageMainForm.cs
+++ DBDBDIB/DBDBDIB/MessageMainForm.cs
@@ -7,33 +7,152 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace DBDBDIB
 {
     public partial class MessageMainForm : Form
     {
-        String msgDetails = "{0,-10}{1, -20}{2, 20}";
+        String msgDetails = "{0,-10}{1, 15}{2, 30}";
+        DBManager DBmanager = DBManager.GetInstance();
+        UserInfo UserManager = UserInfo.Getinstance();
+        List<string> ls = new List<string>();
+
+        enum Search
+        {
+            보낸사람,
+            제목,
+            내용
+        }
+        private Search _selectedSearch;
+        string SearchQuery;
+
         public MessageMainForm()
         {
             InitializeComponent();
         }
 
-        private void MessageMainForm_Load(object sender, EventArgs e)
+        private void LoadMessage()
         {
+            listBox1.Items.Clear();
+            ls.Clear();
             listBox1.Items.Add(String.Format(msgDetails, "읽음유무", "보낸사람", "제목"));
 
-            listBox1.Items.Add(String.Format(msgDetails, "읽음", "재무팀", "입금내역서 안내"));
+            UserManager.Id = "5500000"; // 나중에 지워야함!!!!!!!!
+            string query = "SELECT * FROM 쪽지 join Employee on 쪽지.보낸사람 = Employee.identification WHERE 받는사람 = " + UserManager.Id;
+            MySqlDataReader rdr = DBmanager.select(query);
+            while (rdr.Read())
+            {
+                string row = String.Format(msgDetails, rdr["수신확인"].ToString(), rdr["name"].ToString(), rdr["제목"].ToString());
+                string idRow = rdr["id"].ToString(); // 새로운 폼 열 때 넘겨주기 위한 id list 저장
+                ls.Add(idRow);
+                listBox1.Items.Add(row);
+                //Console.WriteLine(rdr["제목"].ToString());
+            }
+
+            foreach (string v in ls)
+            {
+                Console.WriteLine(v);
+            }
+            rdr.Close();
+        }
+        private void LoadMessage(string query)  //검색할 때 필요한 메서드
+        {
+            listBox1.Items.Clear();
+            ls.Clear();
+            listBox1.Items.Add(String.Format(msgDetails, "읽음유무", "보낸사람", "제목"));
+
+            UserManager.Id = "5500000"; // 나중에 지워야함!!!!!!!!
+            MySqlDataReader rdr = DBmanager.select(query);
+            while (rdr.Read())
+            {
+                string row = String.Format(msgDetails, rdr["수신확인"].ToString(), rdr["name"].ToString(), rdr["제목"].ToString());
+                string idRow = rdr["id"].ToString(); // 새로운 폼 열 때 넘겨주기 위한 id list 저장
+                ls.Add(idRow);
+                listBox1.Items.Add(row);
+            }
+
+            foreach (string v in ls)
+            {
+                Console.WriteLine(v);
+            }
+            rdr.Close();
+        }
+
+        private void MessageMainForm_Load(object sender, EventArgs e)
+        {
+            LoadMessage();
         }
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
             Console.WriteLine("Click");
+            if (radioBtnContent.Checked == false && radioBtnTitle.Checked == false && radioBtnWhoSent.Checked == false)
+                return; // 아무것도 선택 안되었을 때
+            
+            switch (_selectedSearch)
+            {
+                case Search.보낸사람:
+                    Console.WriteLine("보낸사람을 기준으로 검색");
+                    string whoSent = txtSearchWhoSent.Text;
+                    SearchQuery = String.Format("SELECT * FROM 쪽지 join Employee on 쪽지.보낸사람 = Employee.identification " +
+                        "WHERE 받는사람 = {0} AND name LIKE '%{1}%'",UserManager.Id, whoSent);
+                    txtSearchWhoSent.Text = "";
+                    break;
+                case Search.제목:
+                    Console.WriteLine("제목을 기준으로 검색");
+                    string title = txtSearchTitle.Text;
+                    SearchQuery = String.Format("SELECT * FROM 쪽지 join Employee on 쪽지.보낸사람 = Employee.identification " +
+                        "WHERE 받는사람 = {0} AND 제목 LIKE '%{1}%'", UserManager.Id, title);
+                    txtSearchTitle.Text = "";
+                    break;
+                case Search.내용:
+                    Console.WriteLine("내용을 기준으로 검색");
+                    string content = txtSearchContent.Text;
+                    SearchQuery = String.Format("SELECT * FROM 쪽지 join Employee on 쪽지.보낸사람 = Employee.identification " +
+                        "WHERE 받는사람 = {0} AND 내용 LIKE '%{1}%'", UserManager.Id, content);
+                    txtSearchContent.Text = "";
+                    break;
+            }
+
+            Console.WriteLine(SearchQuery);
+            LoadMessage(SearchQuery);
+
         }
 
         private void btnNewMsg_Click(object sender, EventArgs e)
         {
             MessageWriteForm newForm = new MessageWriteForm();
             newForm.Show();
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listBox1.SelectedIndex == 0)
+                return;
+            Console.WriteLine(ls[listBox1.SelectedIndex-1]);
+            MessageDetail newForm = new MessageDetail(ls[listBox1.SelectedIndex - 1]);
+            newForm.Show();
+            //읽음으로 Update 후 폼 다시 로드
+
+            string query = "UPDATE 쪽지 SET 수신확인 = '읽음' WHERE id = " + ls[listBox1.SelectedIndex - 1];
+            DBmanager.DBquery(query);
+            LoadMessage();
+        }
+
+        private void radioBtnWhoSent_CheckedChanged(object sender, EventArgs e)
+        {
+            this._selectedSearch = Search.보낸사람;
+        }
+
+        private void radioBtnContent_CheckedChanged(object sender, EventArgs e)
+        {
+            this._selectedSearch = Search.내용;
+        }
+
+        private void radioBtnTitle_CheckedChanged(object sender, EventArgs e)
+        {
+            this._selectedSearch = Search.제목;
         }
     }
 }
