473b957a3131230246134f6eb71e00fcfa938a8e
은주 김 <rladmswn625@naver.com>
2020-11-27 09:03:04 +0900

    2020.11.27 김은주 결재리스트

--- DBDBDIB/DBDBDIB/ApprovalListForm.cs
+++ DBDBDIB/DBDBDIB/ApprovalListForm.cs
@@ -14,13 +14,12 @@ namespace DBDBDIB
     public partial class ApprovalListForm : Form
     {
         private string what_Appr = ""; //불러올 테이블 조건
+        private string query = "";
         int id = Convert.ToInt32(UserInfo.Getinstance().Id); //로그인 계정
         public ApprovalListForm()
         {
             InitializeComponent();
             combo_Appr.Text = "";
-            dgv_MyApprList.Columns.Clear();
-            dgv_MyApprList.Rows.Clear();
         }
 
         private void combo_Appr_SelectedIndexChanged(object sender, EventArgs e)
@@ -28,23 +27,27 @@ namespace DBDBDIB
             switch (combo_Appr.Text) //콤보박스 선택 변경마다 조건 변경
             {
                 case "모든 결재 내역":
-                    what_Appr = "기안자 ='"+ id + "'"; 
+                    what_Appr = "기안자 ='"+ id + "'";
+                    query = "SELECT 기안자,제목,내용,관련업무,코멘트,진행상황,결재자수,제1결재자,제2결재자,제3결재자,기안자시간,1승인시간,2승인시간,3승인시간,반려메모 FROM 결재 WHERE " + what_Appr;
                     //내가 기안자인 모든 결재 내역 :: 기안자 = 로그인한 계정
                     break;
                 case "결재 중 내역":
                     what_Appr = "기안자 ='" + id + "' AND 진행상황 < 결재자수";
+                    query = "SELECT 기안자,제목,내용,관련업무,코멘트,제1결재자,제2결재자,제3결재자,기안자시간,1승인시간,2승인시간,3승인시간,반려메모 FROM 결재 WHERE " + what_Appr;
                     //내가 기안자인 결재 중 내역 :: 기안자 = 로그인한 계정 and 진행상황 != 결재자수
                     break;
                 case "결재 완료 내역":
                     what_Appr = "기안자 ='" + id + "' AND 진행상황 = 결재자수";
+                    query = "SELECT 기안자,제목,내용,관련업무,코멘트,제1결재자,제2결재자,제3결재자,기안자시간,1승인시간,2승인시간,3승인시간 FROM 결재 WHERE " + what_Appr;
                     //내가 기안자인 결재 완료 내역 :: 기안자 = 로그인한 계정 and 진행상황 = 결재자수
                     break;
                 case "결재할 내역":
-                    what_Appr = "진행상황 < 결재자수 AND (제1결재자 = '" + id + "' OR 제2결재자 = '" + id + "' OR 제3결재자 = '" + id + "')";  
+                    what_Appr = "진행상황 < 결재자수 AND ((진행상황 = 0 AND 제1결재자 = '" + id + "') OR" +
+                        "(진행상황 = 1 AND 제2결재자 = '" + id + "') OR (진행상황 = 2 AND 제3결재자 = '" + id + "'))";
+                    query = "SELECT 기안자,제목,내용,관련업무,코멘트,제1결재자,제2결재자,제3결재자,기안자시간,1승인시간,2승인시간,3승인시간,반려메모 FROM 결재 WHERE " + what_Appr;
                     //내가 결재할 차례인 모든 결재 내역 :: 결재자1 = 로그인한 계정 || 결재자2 = 로그인한 계정 || 결재자3 = 로그인한 계정
                     break;
             }
-            string query = "SELECT * FROM 결재 WHERE " + what_Appr ;
             MySqlDataReader rdr = DBManager.GetInstance().select(query); //테이블 가져오기
 
             DataTable dt = new DataTable();
